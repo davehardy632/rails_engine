7999d1cf086c3d41cd8d9489fbcb9f17600b0526
David Hardy <davehardy632@gmail.com>
2019-06-24 18:53:51 -0600

    created models, model tests, database tables and factories for tables

---
+++ spec/models/invoice_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Invoice, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+    it {should validate_presence_of :status}
+  end
+
+  describe "relationships" do
+    it {should belong_to :customer}
+    it {should belong_to :merchant}
+    it {should have_many :transactions}
+  end
+end
